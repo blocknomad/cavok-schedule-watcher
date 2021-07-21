07ecbd3f9ae7f50b0128ba06308f1da1c5300831
Yuri Jean Fabris <yuri.fabris@gmail.com>
2021-07-21 02:45:32 +0000

    feat: add cronjob and load env vars

--- index.mjs
+++ index.mjs
@@ -1,59 +1,76 @@
 import puppeteer from "puppeteer";
+import { CronJob } from "cron";
 import fs from "fs";
 
-const LOGIN_PAGE_URL = "";
-const SCHEDULE_PAGE_URL = "";
+const LOGIN_PAGE_URL = process.env.LOGIN_PAGE_URL;
+const SCHEDULE_PAGE_URL = process.env.SCHEDULE_PAGE_URL;
 
-const USERNAME = "";
-const PASSWORD = "";
-const TYPE = "";
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const TYPE = process.env.TYPE;
 
-(async () => {
-  const browser = await puppeteer
+const x = async () => {
+  let browser;
+
+  try {
+    browser = await puppeteer
     .launch({
-      headless: false,
+      headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     })
-  const page = await browser.newPage()
 
-  await page.goto(LOGIN_PAGE_URL, { waitUntil: 'load' })
+    const page = await browser.newPage()
 
-  await page.type("[name=username]", USERNAME);
-  await page.type("[name=password]", PASSWORD);
-  await page.select("[name=tipo]", TYPE);
-  await page.click("[type=submit]");
+    await page.goto(LOGIN_PAGE_URL, { waitUntil: 'load' })
 
-  await page.goto(SCHEDULE_PAGE_URL, { waitUntil: 'load' });
+    await page.type("[name=username]", USERNAME);
+    await page.type("[name=password]", PASSWORD);
+    await page.select("[name=tipo]", TYPE);
+    await page.click("[type=submit]");
 
-  const ax = await page.evaluate(() => {
-    const TIME_SLOTS = ['08:30', '10:00', '11:30', '13:00', '14:30', '16:00'];
+    await page.goto(SCHEDULE_PAGE_URL, { waitUntil: 'load' });
 
-    // get schedule rows
-    const rows = document.querySelectorAll('td:first-child strong');
-    
-    // filter next 14 days C150 schedule rows  
-    const rowsC150 = [...rows].filter(element => element.innerText === 'C150-L').slice(0, 25);
-    const availableTimeSlots = [];
+    const availableTimeSlots = await page.evaluate(() => {
+      const TIME_SLOTS = ['08:30', '10:00', '11:30', '13:00', '14:30', '16:00'];
+      const EVALUATE_NEXT_N_DAYS = 25;
+      const ACFT = 'C150-L';
 
-    rowsC150.map(rowC150 => {
-      // get time slots from 8:30 to 16:00
-      const timeSlots = [...rowC150.parentElement.parentElement.parentElement.children].slice(2, -2);
+      // get schedule rows
+      const rows = document.querySelectorAll('td:first-child strong');
 
-      // find available time slots
-      timeSlots.map((timeSlot, i) => {
-        if (timeSlot.querySelector('p:first-child').innerText.toUpperCase() === 'AVALIABLE') {
-          availableTimeSlots.push({
-            date: rowC150.closest('table').children[0].children[0].children[0].childNodes[0].textContent.trim(),
-            time: TIME_SLOTS[i],
-            status: timeSlot.querySelector('p:first-child').innerText.toUpperCase()
-          })
-        }
+      // filter next N days acft schedule rows  
+      const rowsAcft = [...rows].filter(element => element.innerText === ACFT).slice(0, EVALUATE_NEXT_N_DAYS);
+      const availableTimeSlots = [];
+
+      rowsAcft.map(rowAcft => {
+        // get time slots from 8:30 to 16:00
+        const timeSlots = [...rowAcft.parentElement.parentElement.parentElement.children].slice(2, -2);
+
+        // find available time slots
+        timeSlots.map((timeSlot, i) => {
+          if (timeSlot.querySelector('p:first-child').innerText.toUpperCase() === 'AVALIABLE') {
+            availableTimeSlots.push({
+              date: rowAcft.closest('table').children[0].children[0].children[0].childNodes[0].textContent.trim(),
+              time: TIME_SLOTS[i],
+              status: timeSlot.querySelector('p:first-child').innerText.toUpperCase()
+            })
+          }
+        });
       });
+
+      return availableTimeSlots
     });
 
-    return availableTimeSlots
-  });
+    console.log(new Date(), availableTimeSlots)
 
-  console.log('ax', ax)
-  // fs.writeFile(`./${new Date().getMilliseconds()}.json`, JSON.stringify(ax, null, 2), err => { if (err) throw err });
-})()
+    // fs.writeFile(`./${new Date().getMilliseconds()}.json`, JSON.stringify(ax, null, 2), err => { if (err) throw err });
+  } catch (error) {
+    console.log(error)
+  } finally {
+    await browser.close();
+  }
+}
+
+const job = new CronJob('*/10 * * * *', x, null, true, 'America/Sao_Paulo');
+
+job.start();
